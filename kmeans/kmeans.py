5f05fdba298d81c28c4109dbd4c87c58a5a18eaa
virajvchavan <virajc@live.com>
2018-03-31 23:28:57 +0530

    kmeans is now complete

--- kmeans/kmeans.py
+++ kmeans/kmeans.py
@@ -1,25 +1,33 @@
-import copy, math, pdb
+import copy, math, pdb, csv, random
 
 k = input("Enter k:")
 
 def read_data_set():
-	return [[2,3],[8,4],[4,15],[1,13],[8,3],[9,5],[6,4]]
+	with open('file.csv', 'rU') as f:
+		return [list(map(int, rec)) for rec in csv.reader(f, delimiter=',')]
 
 def find_initial_means(data_set, k):
-	return [[2, 3],[8,3],[6,4]]
+	means = []
+	i = 0
+	while i < k:
+		rand_mean = data_set[random.randint(0, len(data_set) - 1)]
+		if(rand_mean not in means):
+			means.append(rand_mean)
+			i += 1
+		else:
+			i -= 1
+	return means
 
 def recalculate_means(clusters):
 	means = []
 	for cluster in clusters:
-		if(len(cluster) > 0):
-			total_x = 0
-			total_y = 0
-			for point in cluster:
-				total_x += point[0]
-				total_y += point[1]
-			# pdb.set_trace()
-			mean = [total_x/len(cluster), total_y/len(cluster)]
-			means.append(mean)
+		total_x = 0
+		total_y = 0
+		for point in cluster:
+			total_x += point[0]
+			total_y += point[1]
+		mean = [round((float(total_x)/len(cluster)),2), round(float(total_y)/len(cluster), 2)]
+		means.append(mean)
 	return means
 
 def calculate_distance(point1, point2):
@@ -30,16 +38,17 @@ def assign_clusters(data_set, means, k):
 	for point in data_set:
 		min_distance = calculate_distance(means[0], point)
 		min_distance_index = 0
-		for index, mean in enumerate(means[1:]):
-			if(calculate_distance(mean, point) < min_distance):
-				min_distance_index = index
+		for i in range(1, len(means)):
+			if(calculate_distance(means[i], point) < min_distance):
+				min_distance_index = i
+				min_distance = calculate_distance(means[i], point)
 		clusters[min_distance_index].append(point)
-		# pdb.set_trace()
 	return clusters
 
 data_set = read_data_set()
 means = find_initial_means(data_set, k)
 old_means = copy.deepcopy(means)
+clusters = []
 while True:
 	print("Means: " + str(means))
 	clusters = assign_clusters(data_set, means, k)
@@ -50,6 +59,9 @@ while True:
 		old_means = copy.deepcopy(means)
 
 print("Final Means: " + str(means))
+print("Clusters: \n............................................." )
+for cluster in clusters:
+	print(cluster)
 
 
 
