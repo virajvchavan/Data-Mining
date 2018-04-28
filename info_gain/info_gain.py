42bd039181210bf2c4963ef2fe0bafb6d0dce448
virajvchavan <viraj@joshsoftware.com>
2018-04-28 21:29:34 +0530

    read dataset from file

--- info_gain/info_gain.py
+++ info_gain/info_gain.py
@@ -1,1 +1,18 @@
-#easy, i guess
+import math
+
+def read_dataset(filename):
+	data = {}
+	lines = open(filename).readlines()
+	data['headers'] = lines[0].split(',')
+	data['no_of_tuples'] = len(lines) - 1
+	for h in data['headers']:
+		data[h] = []
+	# so far:  {'outlook': [], 'temp': [], 'count_tuples': 14, 'humidity': [], 'headers': ['outlook', 'temp', 'humidity', 'windy', 'play\n'], 'windy': [], 'play\n': []}	
+	for line in lines[1:]:
+		line = line.split(',')
+		for i in range(len(data['headers'])):
+			data[data['headers'][i]].append(line[i])
+	return data
+
+data = read_dataset('tennis.csv')
+print data
