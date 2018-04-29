257807d65652a4e5cc136e553e0b726f29b6dc84
virajvchavan <viraj@joshsoftware.com>
2018-04-29 18:56:03 +0530

    almost completed

--- bayes_classifier/bayes_classifier.py
+++ bayes_classifier/bayes_classifier.py
@@ -22,7 +22,11 @@ def mean(values):
 	return(sum(values)/float(len(values)))
 
 def std(values):
-	return 2
+	avg = mean(values)
+	var = 0
+	for val in values:
+		var += pow(val - avg, 2)
+	return math.sqrt(var/(len(values) - 1))
 
 def get_summary(instances):
 	summary =  [(mean(a), std(a)) for a in zip(*instances)]
@@ -36,12 +40,43 @@ def summarize_by_class(train):
 		summaries[class_value] = get_summary(instances)
 	return summaries
 
+def get_probability(x, mean, std):
+	e = math.exp(-(math.pow(x-mean, 2)/(2 * math.pow(std, 2))))
+	return (1 / (math.sqrt(2*math.pi) * std)) * e
+
+def get_class_probabs(summaries, row):
+	class_probabs = {}
+	for class_name, class_summaries in summaries.items():
+		class_probabs[class_name] = 1
+		for i in range(len(class_summaries)):
+			x  = row[i]
+			mean, std = class_summaries[i]
+			class_probabs[class_name] *= get_probability(x, mean, std)
+	return class_probabs
+
+def predict(summaries, row):
+	class_probabs = get_class_probabs(summaries, row)
+	probab = -1
+	predicted_class = None
+	for class_name, class_probab in class_probabs.items():
+		if predicted_class == None or probab < class_probab:
+			probab = class_probabs[class_name]
+			predicted_class = class_name
+	return predicted_class
+
+def get_predictions(summaries, test):
+	predictions = []
+	for row in test:
+		predictions.append(predict(summaries, row))
+	return predictions
+
 def classify():
 	data =  read_dataset('diabetis.csv')
 	split_ratio = 0.70
 	train, test = split_data(data, split_ratio)
 	summaries = summarize_by_class(train)
-	print summaries
+	predictions = get_predictions(summaries, test)
+	print(predictions)
 
 classify()
 
