d7944598c8c060378bd29dc6bdd111029726083b
kauziishere <kaustubhjoshi1411@gmail.com>
2018-05-07 17:56:59 +0530

    minor

--- hierarchical_clustering/hierarchical_clustering.py
+++ hierarchical_clustering/hierarchical_clustering.py
@@ -4,13 +4,13 @@ def read_data():
 	dataset = {}
 	dataset['rows'] = ['a', 'b', 'c', 'd', 'e', 'f']
 	dataset['mat']  = [
-											[0,662,877,255,412,996],
-											[662,0,295,468,268,400],
-											[877,295,0,754,264,138],
-											[255,468,754,0,219,869],
-											[412,268,264,219,0,669],
-											[996,400,138,869,669,0]
-										]
+						[0,662,877,255,412,996],
+						[662,0,295,468,268,400],
+						[877,295,0,754,264,138],
+						[255,468,754,0,219,869],
+						[412,268,264,219,0,669],
+						[996,400,138,869,669,0]
+					  ]
 	return dataset
 
 def print_dataset(dataset, complete = False):
@@ -38,7 +38,6 @@ def find_position_of_min_distance(matrix):
 				min_x = x
 				min_y = y
 	return [min_x, min_y]
-
 
 def merge_clusters(dataset, min_position):
 	matrix = dataset['mat']
